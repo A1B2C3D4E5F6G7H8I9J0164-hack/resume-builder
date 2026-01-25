e71c666a9ad69897ac2f523c87c2c295cf06e69b
aditya rana <adityarana09102006@gmail.com>
2026-01-25 19:01:05 +0530

    chore: Regenerate Next.js development build artifacts due to build process changes.

--- frontend/.next/dev/server/middleware-build-manifest.js
+++ frontend/.next/dev/server/middleware-build-manifest.js
@@ -3,22 +3,9 @@ globalThis.__BUILD_MANIFEST = {
     "/_app": []
   },
   "devFiles": [],
-  "polyfillFiles": [
-    "static/chunks/4755b_next_dist_build_polyfills_polyfill-nomodule.js"
-  ],
+  "polyfillFiles": [],
   "lowPriorityFiles": [],
-  "rootMainFiles": [
-    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_d68740f7._.js",
-    "static/chunks/4755b_next_dist_compiled_react-dom_fc72d426._.js",
-    "static/chunks/4755b_next_dist_compiled_react-server-dom-turbopack_9b43f391._.js",
-    "static/chunks/4755b_next_dist_compiled_next-devtools_index_474dd529.js",
-    "static/chunks/4755b_next_dist_compiled_0bc7fc72._.js",
-    "static/chunks/4755b_next_dist_client_cd585a08._.js",
-    "static/chunks/4755b_next_dist_1b6ca61e._.js",
-    "static/chunks/4755b_@swc_helpers_cjs_a156ddd8._.js",
-    "static/chunks/Downloads_resume_genrator_frontend_a0ff3932._.js",
-    "static/chunks/turbopack-Downloads_resume_genrator_frontend_a2253954._.js"
-  ]
+  "rootMainFiles": []
 };
 globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
 "/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
