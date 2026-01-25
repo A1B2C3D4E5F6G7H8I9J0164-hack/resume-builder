411609881a3479d36dcb647a8ed1e20a21acbf91
aditya rana <adityarana09102006@gmail.com>
2026-01-25 20:25:50 +0530

    chore: Update Next.js development server internal manifests and trace logs.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -20,13 +20,18 @@ const nextConfig: NextConfig = {
     },
 
     async rewrites() {
+        const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
+        if (!apiUrl) return [];
+
         return [
             {
                 source: "/api/:path*",
-                destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
+                destination: `${apiUrl}/api/:path*`,
             },
         ];
-    },
+    }
+
 };
 
 export default nextConfig;
